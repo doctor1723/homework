89d9ceed578f50b254444053b4208c58c41f0b78
doctor1723 <doctor240290@mail.ru>
2024-07-23 19:02:49 +0300

    Add files via upload

---
+++ Homework7.py
@@ -0,0 +1,23 @@
+
+amout_1=sum([5,3,3,5,4])
+number_of_ratings_1=len([5,3,3,5,4])
+average_score_1=amout_1/number_of_ratings_1
+students_1='Aaron'
+amout_2=sum([2,2,2,3])
+number_of_ratings_2=len([2,2,2,3])
+average_score_2=amout_2/number_of_ratings_2
+students_2='Bilbo'
+amout_3=sum([4,5,5,2])
+number_of_ratings_3=len([4,5,5,2])
+average_score_3=amout_3/number_of_ratings_3
+students_3='Johhny'
+amout_4=sum([4,4,3])
+number_of_ratings_4=len([4,4,3])
+average_score_4=amout_4/number_of_ratings_4
+students_4='Khendrik'
+amout_5=sum([5,5,5,4,5])
+number_of_ratings_5=len([5,5,5,4,5])
+average_score_5=amout_5/number_of_ratings_5
+students_5='Steve'
+print({students_1: average_score_1, students_2:average_score_2,
+       students_3: average_score_3,students_4: average_score_4, students_5:average_score_5})
